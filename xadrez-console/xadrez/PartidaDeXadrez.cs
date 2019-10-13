071886f54a334ec2f6f58eb22d67acefa1e39958
Filipe Moreira <filimor@posteo.com.br>
2019-10-13 18:52:13 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -118,6 +118,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaespecial promocao
+            if(p is Peao)
+            {
+                if((p.Cor==Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    _pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    _pecas.Add(dama);
+                }
+            }
+
             Xeque = EstaEmXeque(Adversaria(JogadorAtual));
             if (TesteXequeMate(Adversaria(JogadorAtual)))
             {
@@ -130,7 +145,7 @@ namespace xadrez
             }
 
             //#jogadaespecial en passant
-            Peca p = Tab.Peca(destino);
+            
             VulneravelEnPassant = p is Peao && destino.Linha == origem.Linha -2 || destino.Linha == origem.Linha + 2 ? p : null;
 
         }
